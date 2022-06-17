9cb1ed409f76d3c77bf79b1d62fae3ba5482aee6
Matthew Thorning <matthew@ometria.com>
2022-06-17 16:50:20 +0100

    s/acme/mthornin-om in hidden files

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "@mthorning-om/core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/mthorning-om-core/"
             ),
           },
         ],
